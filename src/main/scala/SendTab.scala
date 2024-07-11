0d76e08b2021f80589b3b7a4b12ed1e9439b9b12
rxxuzi <75666622+rxxuzi@users.noreply.github.com>
2024-07-11 15:40:04 +0900

    Update: send tab

--- src/main/scala/SendTab.scala
+++ src/main/scala/SendTab.scala
@@ -1,28 +1,48 @@
-import javafx.collections.FXCollections
-import javafx.geometry.Insets
+import content.{RemoteFile, Tree}
+import javafx.application.Platform
+import javafx.geometry.{Insets, Orientation}
 import javafx.scene.control.*
-import javafx.scene.layout.{HBox, VBox}
+import javafx.scene.control.cell.TextFieldTreeCell
+import javafx.scene.layout.{HBox, Priority, VBox}
 import javafx.stage.FileChooser
+import javafx.util.StringConverter
 import ssh.SSHManager
 
 import java.io.File
+import scala.jdk.CollectionConverters.*
+import scala.util.Try
 
 class SendTab(sshManager: SSHManager, updateTitle: () => Unit) {
   private val statusArea = new TextArea()
   statusArea.setEditable(false)
   statusArea.setPrefRowCount(10)
 
-  private val remoteDirComboBox = new ComboBox[String](FXCollections.observableArrayList())
-  remoteDirComboBox.setPromptText("Select Remote Directory")
-  remoteDirComboBox.setEditable(true)
+  private val fileTreeView = new TreeView[RemoteFile]()
+  fileTreeView.setShowRoot(false)
+  fileTreeView.setCellFactory(_ => new TextFieldTreeCell[RemoteFile](new StringConverter[RemoteFile]() {
+    override def toString(rf: RemoteFile): String = rf.name
+    override def fromString(string: String): RemoteFile = null // Not used for this example
+  }))
 
-  def getContent: VBox = {
-    val localFilePathField = new TextField()
+  private val showAllFilesRadio = new RadioButton("Show All Files")
+  private val showVisibleFilesRadio = new RadioButton("Show Visible Files Only")
+  private val toggleGroup = new ToggleGroup()
+  showAllFilesRadio.setToggleGroup(toggleGroup)
+  showVisibleFilesRadio.setToggleGroup(toggleGroup)
+  showVisibleFilesRadio.setSelected(true)
+
+  private val localFilePathField = new TextField()
+  private val remoteFilePathField = new TextField()
+  localFilePathField.setPrefWidth(300)
+  remoteFilePathField.setPrefWidth(300)
+
+  def getContent: SplitPane = {
     localFilePathField.setPromptText("Local file path")
-    val chooseFileButton = new Button("Choose File")
+    remoteFilePathField.setPromptText("Remote directory path")
 
+    val chooseFileButton = new Button("Choose File")
     val sendButton = new Button("Send File")
-    val listFilesButton = new Button("List Remote Files")
+    val refreshButton = new Button("Refresh")
 
     chooseFileButton.setOnAction(_ => {
       val fileChooser = new FileChooser()
@@ -35,49 +55,109 @@ class SendTab(sshManager: SSHManager, updateTitle: () => Unit) {
 
     sendButton.setOnAction(_ => {
       if (sshManager.isConnected) {
-        sshManager.withSSH { ssh =>
-          val localFile = new File(localFilePathField.getText)
-          val remoteDir = remoteDirComboBox.getValue
-          val remotePath = if (remoteDir == "./") localFile.getName else s"$remoteDir/${localFile.getName}"
-          ssh.send(localFile, remotePath)
-          statusArea.appendText(s"File sent: ${localFile.getAbsolutePath} -> $remotePath\n")
-          updateRemoteDirList()
-        }.recover {
-          case ex => statusArea.appendText(s"Send failed: ${ex.getMessage}\n")
+        val localFile = new File(localFilePathField.getText)
+        val remotePath = remoteFilePathField.getText
+        if (localFile.exists() && remotePath.nonEmpty) {
+          sshManager.withSSH { ssh =>
+            val remoteFilePath = s"$remotePath/${localFile.getName}"
+            ssh.send(localFile, remoteFilePath)
+            statusArea.appendText(s"File sent: ${localFile.getAbsolutePath} -> $remoteFilePath\n")
+            updateFileTree()
+          }.recover {
+            case ex => statusArea.appendText(s"Send failed: ${ex.getMessage}\n")
+          }
+        } else {
+          statusArea.appendText("Please select a valid local file and remote directory.\n")
         }
       } else {
         statusArea.appendText("Not connected. Please connect to SSH first.\n")
       }
     })
 
-    listFilesButton.setOnAction(_ => updateRemoteDirList())
+    refreshButton.setOnAction(_ => updateFileTree())
 
-    val content = new VBox(10)
-    content.setPadding(new Insets(10))
-    content.getChildren.addAll(
-      new HBox(10, new Label("Local File:"), localFilePathField, chooseFileButton),
-      new HBox(10, new Label("Remote Dir:"), remoteDirComboBox),
-      new HBox(10, sendButton, listFilesButton),
-      statusArea
+    toggleGroup.selectedToggleProperty().addListener((_, _, newValue) => {
+      if (newValue != null) {
+        updateFileTree()
+      }
+    })
+
+    fileTreeView.getSelectionModel.selectedItemProperty().addListener((_, _, newValue) => {
+      if (newValue != null) {
+        remoteFilePathField.setText(newValue.getValue.fullPath)
+      }
+    })
+
+    val topPane = new VBox(10)
+    topPane.setPadding(new Insets(10))
+    topPane.getChildren.addAll(
+      new HBox(10, new Label("Local Path:"), localFilePathField, chooseFileButton),
+      new HBox(10, new Label("Remote Path:"), remoteFilePathField),
+      new HBox(10, sendButton)
     )
 
-    content
+    val leftPane = new VBox(10)
+    leftPane.setPadding(new Insets(10))
+    leftPane.getChildren.addAll(
+      new HBox(10, new Label("Remote Directories:"), refreshButton),
+      new HBox(10, showAllFilesRadio, showVisibleFilesRadio),
+      fileTreeView
+    )
+    VBox.setVgrow(fileTreeView, Priority.ALWAYS)
+
+    val rightPane = new VBox(10)
+    rightPane.setPadding(new Insets(10))
+    rightPane.getChildren.addAll(
+      new Label("Terminal:"),
+      statusArea
+    )
+    VBox.setVgrow(statusArea, Priority.ALWAYS)
+
+    val bottomPane = new SplitPane()
+    bottomPane.setOrientation(Orientation.HORIZONTAL)
+    bottomPane.getItems.addAll(leftPane, rightPane)
+    bottomPane.setDividerPositions(0.6)
+
+    val mainSplitPane = new SplitPane()
+    mainSplitPane.setOrientation(Orientation.VERTICAL)
+    mainSplitPane.getItems.addAll(topPane, bottomPane)
+    mainSplitPane.setDividerPositions(0.3)
+
+    updateFileTree()
+
+    mainSplitPane
   }
 
-  private def updateRemoteDirList(): Unit = {
+  private def updateFileTree(): Unit = {
     if (sshManager.isConnected) {
       sshManager.withSSH { ssh =>
-        val homeDir = ssh.exec("echo $HOME").trim
-        val directories = "./" :: ssh.listFiles(homeDir)
-          .filter(_.isDir)
-          .map(_.name)
-          .filter(dir => !dir.startsWith(".") && !List("..").contains(dir))
-        val items = FXCollections.observableArrayList(directories: _*)
-        remoteDirComboBox.setItems(items)
-        remoteDirComboBox.setValue("./")
-        statusArea.appendText("Remote directory list updated.\n")
+        Try {
+          val homeDir = ssh.exec("echo $HOME").trim
+          val showAllFiles = showAllFilesRadio.isSelected
+          val fileList = ssh.exec(s"find $homeDir -type d -o -type f").split("\n").filter(_.nonEmpty)
+          val rootItem = new TreeItem[RemoteFile](RemoteFile("~", homeDir, isDirectory = true))
+          rootItem.setExpanded(true)
+
+          fileList.foreach { path =>
+            val relativePath = path.replace(homeDir, "").stripPrefix("/")
+            if (showAllFiles || !relativePath.split("/").exists(_.startsWith("."))) {
+              Tree.addPathToTree(rootItem, relativePath, path, homeDir)
+            }
+          }
+
+          Platform.runLater(() => {
+            fileTreeView.setRoot(rootItem)
+            statusArea.appendText("Remote file tree updated.\n")
+          })
+        }.recover {
+          case ex => Platform.runLater(() => {
+            statusArea.appendText(s"Failed to list remote files: ${ex.getMessage}\n")
+          })
+        }
       }.recover {
-        case ex => statusArea.appendText(s"Failed to update remote directory list: ${ex.getMessage}\n")
+        case ex => Platform.runLater(() => {
+          statusArea.appendText(s"SSH operation failed: ${ex.getMessage}\n")
+        })
       }
     } else {
       statusArea.appendText("Not connected. Please connect to SSH first.\n")
