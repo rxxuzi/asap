f0a3ebd0d87f931daea45591bc48064d18e5e231
rxxuzi <75666622+rxxuzi@users.noreply.github.com>
2024-07-15 23:27:13 +0900

    Add: Style.scala

---
+++ src/main/scala/style/Style.scala
@@ -0,0 +1,49 @@
+package style
+
+import global.Config
+import javafx.scene.Scene
+
+object Style {
+  val dark: (String, String) = ("#2C2C2C", "#E0E0E0")
+  val light: (String, String) = ("#FFFFFF", "#333333")
+  
+  private val dir = "css"
+  private var currentStyle: CSS = if (Config.dark) CSS.DARK else CSS.LIGHT
+
+  private val styles = Map(
+    CSS.MAIN -> "main.css",
+    CSS.LIGHT -> "light.css",
+    CSS.DARK -> "dark.css"
+  )
+
+  def getCurrentStyle: CSS = currentStyle
+
+  def toggleStyle(scene: Scene): Unit = {
+    currentStyle = if (currentStyle == CSS.LIGHT) CSS.DARK else CSS.LIGHT
+    updateSceneStyle(scene)
+  }
+
+  def updateSceneStyle(scene: Scene): Unit = {
+    scene.getStylesheets.clear()
+    scene.getStylesheets.add(set(CSS.MAIN))
+    scene.getStylesheets.add(set(currentStyle))
+  }
+
+  def set(css: CSS): String = {
+    if (Config.css) {
+      styles.get(css) match {
+        case Some(fileName) =>
+          val resourcePath = s"/$dir/$fileName"
+          val cssUrl = getClass.getResource(resourcePath)
+          if (cssUrl == null) {
+            throw new IllegalArgumentException(s"File not found: $resourcePath")
+          }
+          cssUrl.toExternalForm
+        case None =>
+          throw new IllegalArgumentException(s"No CSS file found for key: $css")
+      }
+    } else {
+      ""
+    }
+  }
+}
